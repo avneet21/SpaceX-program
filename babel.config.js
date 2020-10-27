4d023a0238b82fc96eee7c721c600114fe9f7caf
avneet21 <avneet21000@gmail.com>
2020-10-27 12:44:17 +0530

    Fix for heroku build

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,21 @@
-module.exports  = {
-    // "env": {
-    //   "test": {
-    //     "presets": [
-    //       "@babel/preset-env",
-    //       "@babel/react",
-    //       "next/babel"
-    //     ]
-    //   },
-    //   "development": {
-    //     "presets": [
-    //       "next/babel"
-    //     ]
-    //   }
-    // },
-    "presets": [
-      "@babel/preset-env",
-      "@babel/react",
-      "next/babel"
-    ],
-    "plugins" : ["@babel/plugin-transform-react-jsx"]
-  }
+module.exports = {
+  "env": {
+    "test": {
+      "presets": [
+        "@babel/preset-env",
+        "@babel/react",
+        "next/babel"
+      ]
+    },
+    "development": {
+      "presets": [
+        "next/babel"
+      ]
+    },
+    "production": {
+      "presets": [
+        "next/babel"
+      ]
+    }
+  },
+}
